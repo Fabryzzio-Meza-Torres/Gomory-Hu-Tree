93acb024d2bdb9f08d06c8f0079c705ce1850cf9
FernandoUsurin <83975104+FernandoUs@users.noreply.github.com>
2024-02-24 12:26:12 -0500

    Modify functions

--- main.cpp
+++ main.cpp
@@ -5,15 +5,15 @@ using namespace std;
 
 int main()
 {
-    freopen("input.txt", "r", stdin);
     Graph graph;
     // Leer el grafo desde la entrada estándar
-    graph.read_input_file();
+    graph.read_input_file("input.txt");
     graph.print();
     cout << endl;
     cout << graph.bfs(2, 0) << endl;
     cout << "Gomory-Hu Tree"<<endl;
     graph.mgh();
+    graph.grabar_datos("output.txt");
 
     return 0;
 }
